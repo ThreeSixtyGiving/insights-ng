baaa8c1e789911c29c341d592092c8b7c0d6a958
Michael Wood <michael.wood@opendataservices.coop>
2022-02-18 16:47:27 +0000

    test_browser: Add check for funders page
    Also make sure we definitely test the home page.

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -6,6 +6,8 @@ from flask import url_for
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+from insights.db import Publisher
+
 os.environ['FLASK_ENV'] = 'development'
 BROWSER = os.environ.get('BROWSER', 'ChromeHeadless')
 
@@ -36,6 +38,17 @@ def test_home(browser):
 
 @pytest.mark.usefixtures('live_server')
 def test_javascript_errors_in_page(browser):
-    for log in browser.get_log("browser"):
-        assert "SEVERE" not in log['level'], f"found {log}"
 
+    def check_browser():
+        for log in browser.get_log("browser"):
+            assert "SEVERE" not in log['level'], f"found {log}"
+
+    browser.get(url_for('index', _external=True))
+    check_browser()
+
+    data_url = url_for('data', _external=True)
+
+    for publisher in Publisher.query.filter()[:5]:
+        browser.get("%s/data?funders=%s" % (data_url, publisher.prefix))
+        browser.implicitly_wait(2)
+        check_browser()
